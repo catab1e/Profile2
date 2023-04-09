c899a9a7845c918aad5bf3f3094ed4156d869938
Sary Sary <tilektursunbaiuulu@gmail.com>
2023-04-10 04:25:16 +0600

    remaked and finished left side of 2-section

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -5,10 +5,9 @@ import fon7 from '../../assets/bg/fon7.png'
 
 const Project = (props) => {
     return (
-        <div className='project'>
+        <div className={props.curInd === props.num ? 'project project--selected' : 'project' }>
             <div className='project__img-holder'>
                 <img className='project__img' src={props.imageUrl} alt="" />
-                {console.log(props.imageUrl)}
             </div>
             <div className='project__descr'>{props.name}</div>
         </div>
--- src/components/sections/section-2/SecondSection.jsx
+++ src/components/sections/section-2/SecondSection.jsx
@@ -1,6 +1,6 @@
 import Project from '../../projects/Project';
 import './second-section.scss'
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import fon7 from '../../../assets/bg/fon7.png'
 import fon3 from '../../../assets/bg/fon3.png'
 import fon6 from '../../../assets/bg/fon6.png'
@@ -12,7 +12,7 @@ const arr = [
     },
     {
         name: "project-02",
-        imageUrl: "../../../assets/bg/fon7.png"
+        imageUrl: "assets/bg/fon7.png"
     },
     {
         name: "project-03",
@@ -24,23 +24,51 @@ const arr = [
     },
     {
         name: "project-05",
-        imageUrl: "../../assets/bg/fon6.png"
+        imageUrl: "../../../../assets/bg/fon6.png"
     }
 ]
 
+
+
 const SecondSection = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const scrollBlockRef = useRef(null);
+  
+    useEffect(() => {
+      if (scrollBlockRef.current) {
+        scrollBlockRef.current.scrollTop = currentIndex * 290;
+      }
+    }, [currentIndex]);
+
+
+    const handleUpClick = () => {
+        setCurrentIndex(currentIndex === 0 ? arr.length - 1 : currentIndex - 1);
+        console.log("upped")
+      };
+      
+      const handleDownClick = () => {
+        setCurrentIndex(currentIndex === arr.length - 1 ? 0 : currentIndex + 1);
+        console.log("downed")
+      };
+
     return (
         <div className='second-section'>
             <div className='second-section__block'>
-                <div className='second-section--wrapper'>
-                    {arr.map((obj) => (
-                        <Project name={obj.name} imageUrl={obj.imageUrl}/>
+                <div className='second-section--wrapper' ref={scrollBlockRef}>
+                    {arr.map((obj , index) => (
+                        <Project key={index} num={index} curInd={currentIndex} name={obj.name} imageUrl={obj.imageUrl}/>
                     ))}
+                </div>
+
+                <div className='second-section__buttons'>
+                    <button className='second-section__btn' onClick={handleUpClick}>UP</button>
+                    <button className='second-section__btn' onClick={handleDownClick}>DOWN</button>
                 </div>
             </div>
 
             <div className='second-section__block'>
-
+                <h1>METADATA</h1>
             </div>
         </div> 
     );
