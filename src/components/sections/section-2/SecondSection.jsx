8390657fc0b4830d5cecc0887a7e34075b2e7d1a
Sary Sary <tilektursunbaiuulu@gmail.com>
2023-04-20 05:52:54 +0600

    fixed sidebar

--- src/components/sections/section-2/SecondSection.jsx
+++ src/components/sections/section-2/SecondSection.jsx
@@ -12,19 +12,19 @@ const arr = [
     },
     {
         name: "project-02",
-        imageUrl: "assets/bg/fon7.png"
+        imageUrl: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
     },
     {
         name: "project-03",
-        imageUrl: "../../assets/bg/fon6.png"
+        imageUrl: "https://media.istockphoto.com/id/1405730723/photo/empty-asphalt-road-and-new-year-2023-concept-driving-on-an-empty-road-to-goals-2023-with.jpg?b=1&s=170667a&w=0&k=20&c=xn2OFq_1n9KNM8OdUzIxKBH949UOqe5M4_VQ7XAcvwQ="
     },
     {
         name: "project-04",
-        imageUrl: "../../../assets/bg/fon6.png"
+        imageUrl: "https://emcore.com/wp-content/uploads/2022/09/PIC_hero_bkg.jpg"
     },
     {
         name: "project-05",
-        imageUrl: "../../../../assets/bg/fon6.png"
+        imageUrl: "https://w0.peakpx.com/wallpaper/818/840/HD-wallpaper-heart-whatsapp-dp-love-profile-pic-unique-whatsapp-dp-xoxo-thumbnail.jpg"
     }
 ]
 
@@ -62,8 +62,8 @@ const SecondSection = () => {
                 </div>
 
                 <div className='second-section__buttons'>
-                    <button className='second-section__btn' onClick={handleUpClick}>UP</button>
-                    <button className='second-section__btn' onClick={handleDownClick}>DOWN</button>
+                    <button className='second-section__btn' onClick={handleUpClick}>PREV</button>
+                    <button className='second-section__btn' onClick={handleDownClick}>NEXT</button>
                 </div>
             </div>
 
